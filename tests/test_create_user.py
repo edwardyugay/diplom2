0f28c0b774cab4d209d85b4bfba0b8a866b63f93
EDUARD YUGAY <edwardehwa@gmail.com>
2025-05-21 22:30:07 +0900

    client создаётся внутри каждого теста, фикстура удалена

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -3,9 +3,9 @@ from faker import Faker
 from utils.api_client import APIClient
 
 fake = Faker()
-client = APIClient()
 
 def test_create_unique_user():
+    client = APIClient()
     user = {
         "email": fake.email(),
         "password": "123456",
@@ -14,12 +14,14 @@ def test_create_unique_user():
     response = client.create_user(user)
     assert response.status_code == 200
     assert response.json().get("success") is True
-    # Удаляем пользователя
+
+    # Удаление пользователя после теста
     token = response.json()["accessToken"].split()[-1]
     client.delete_user(token)
 
 @pytest.mark.parametrize("missing_field", ["email", "password", "name"])
 def test_create_user_missing_field(missing_field):
+    client = APIClient()
     user = {
         "email": fake.email(),
         "password": "123456",
